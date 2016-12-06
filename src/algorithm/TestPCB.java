f712ff5b7ce4947ea07b6481d0b437676a3cdf12
zihua <1477765176@qq.com>
2016-12-06 16:03:21 +0800

    操作系统调度算法

--- src/algorithm/TestPCB.java
+++ src/algorithm/TestPCB.java
@@ -3,6 +3,8 @@ package algorithm;
 import java.util.Comparator;
 import java.util.LinkedList;
 import java.util.Queue;
+import java.util.Vector;
+
 
 /**
  * Created by zihua on 16-11-29.
@@ -23,12 +25,14 @@ public class TestPCB {
         queue.add(p4);
         queue.add(p5);
         while(!queue.isEmpty()){
-            PCB t=queue.poll();
-            exe(t,cpuTime);
             Queue<PCB>queue1=new LinkedList<>(queue);
             show(queue1);
-            if(!t.getStatus().equals("finish"))queue.offer(t);
-
+            PCB t=queue.poll();
+            exe(t,cpuTime);
+            if(t.getNeedCp()>0){
+                t.setStatus("wait");
+                queue.offer(t);
+            }
         }
 
     }
@@ -38,17 +42,46 @@ public class TestPCB {
             a.setStatus("Run");
             a.setNeedCp(cpu);
             a.setCpuTime(cpu);
+            System.out.println("Current running......");
+            System.out.println("Id: "+a.getFlag());
+            System.out.println("Priority: "+a.getPriority());
+            System.out.println("Cputime: "+a.getCpuTime());
+            System.out.println("Needcpu: "+a.getNeedCp());
+            System.out.println("Status: "+a.getStatus());
         }
-        else {
-            a.setStatus("finish");
+
+        if(a.getNeedCp()<0){
+            System.out.println("ID "+a.getFlag()+" finish");
         }
-        System.out.println(a);
+        System.out.println("<-------------------------------------------------->\n");
+
     }
 
     public static void show(Queue a){
+        Vector<Integer>id=new Vector<>();
+        Vector<Integer>priority=new Vector<>();
+        Vector<Integer>cputTime=new Vector<>();
+        Vector<Integer>needTime=new Vector<>();
+        Vector<String>status=new Vector<>();
         while(!a.isEmpty()){
-            System.out.println(a.poll());
+            PCB t=(PCB)a.poll();
+            id.add(t.getFlag());
+            priority.add(t.getPriority());
+            cputTime.add(t.getCpuTime());
+            needTime.add(t.getNeedCp());
+            status.add(t.getStatus());
         }
+        if(id.size()>0){
+            System.out.println("<-------------------------------------------------->");
+            System.out.println("Current queue  ");
+            System.out.println("ID          "+id);
+            System.out.println("Priority    "+priority);
+            System.out.println("CpuTime     "+cputTime);
+            System.out.println("NeedTime    "+needTime);
+            System.out.println("Status      "+status);
+
+        }
+
     }
 }
 
@@ -67,62 +100,50 @@ class PCB implements Comparable<PCB>{
         this.needCp=needCp;
         this.status=status;
     }
-
     public int getFlag(){
         return flag;
     }
-
     public int getPriority(){
         return priority;
     }
-
     public int getCpuTime(){
         return cpuTime;
     }
-
     public int getNeedCp(){
         return needCp;
     }
-
     public String getStatus(){
         return status;
     }
-
     public void setStatus(String status){
         this.status=status;
     }
-
     public void setNeedCp(){
         needCp-=cpuTime;
     }
-
     public void setNeedCp(int t){
         needCp-=t;
     }
-
-
     public void setCpuTime(int cpuTime){
         this.cpuTime=cpuTime;
     }
-
     public void setPriority(int priority){
         this.priority=priority;
     }
-
     public void setPriority(){
         priority-=1;
     }
-
     @Override
     public int compareTo(PCB o) {
         if(priority>o.priority)return 1;
         else if(priority<o.priority)return -1;
         return 0;
     }
-
     @Override
     public String toString(){
-        return "进程号："+flag+"  "+"优先级 : "+priority+"  "
-                +"时间片： "+cpuTime+"  "+"还需时间片: "+needCp+"  "+"线程状态: "+status;
+        return flag+" "+priority+" "+cpuTime+" "+needCp+" "+status;
     }
 }
+
+
+
